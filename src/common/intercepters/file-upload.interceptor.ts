314e9ce8ca2035ed0ff93254842d5706a07d45f0
tanvirAlamOmi <tanviralamomi@gmail.com>
2025-05-08 04:32:19 +0600

    fix

--- src/common/intercepters/file-upload.interceptor.ts
+++ src/common/intercepters/file-upload.interceptor.ts
@@ -1,26 +1,26 @@
-// import { FilesInterceptor } from '@nestjs/platform-express';
-// import { diskStorage } from 'multer';
-// import { extname } from 'path';
-// import { BadRequestException } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { BadRequestException } from '@nestjs/common';
 
-// export const ImageFilesInterceptor = (fieldName: string, maxCount: number = 10) =>
-//   FilesInterceptor(fieldName, maxCount, {
-//     storage: diskStorage({
-//       destination: './uploads',
-//       filename: (req, file, callback) => {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-//         const ext = extname(file.originalname);
-//         const filename = `${uniqueSuffix}${ext}`;
-//         callback(null, filename);
-//       },
-//     }),
-//     fileFilter: (req, file, callback) => {
-//       if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-//         return callback(new BadRequestException('Only JPG, JPEG, and PNG files are allowed'), false);
-//       }
-//       callback(null, true);
-//     },
-//     limits: {
-//       fileSize: 20 * 1024 * 1024, // 20MB limit per file
-//     },
-//   });
+export const ImageFilesInterceptor = (fieldName: string, maxCount: number = 10) =>
+  FilesInterceptor(fieldName, maxCount, {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, callback) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = extname(file.originalname);
+        const filename = `${uniqueSuffix}${ext}`;
+        callback(null, filename);
+      },
+    }),
+    fileFilter: (req, file, callback) => {
+      if (!file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+        return callback(new BadRequestException('Only JPG, JPEG, and PNG files are allowed'), false);
+      }
+      callback(null, true);
+    },
+    limits: {
+      fileSize: 20 * 1024 * 1024, // 20MB limit per file
+    },
+  });
